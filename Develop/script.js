4818adb89844f9558a22fc3a36bfd1fe4980e1dd
Jessy Castaneda <jessyc.12@icloud.com>
2024-01-15 12:46:12 -0800

    finished javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,160 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+document.querySelector("#generate").addEventListener("click", writePassword);
 
-//wrtie my most code outside
-// generate pass word function
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = [
+  "!",
+  "%",
+  "&",
+  ",",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  "<",
+  ">",
+  "?",
+  "~",
+];
+var lowercaseABC = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercaseABC = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
 
 function generatePassword() {
-  // collect user options
-  // prompt user for length of password
-  var pwlength = prompt("eneter desire length of password");
-  // verify that password length is between 8 and 128 icluded
-  // logic to generate passwoed
-  // confirm wether or not you want lowercase uppercase numeric and or speical chharacters
+  var confirmLength = prompt(
+    "Your password must be between 8 and 128 characters."
+  );
 
-  // return pass
-  return "password";
+  while (confirmLength < 8 || confirmLength > 128) {
+    alert("Password length must be between 8-128 characters Try again");
+    var confirmLength = prompt(
+      "Your password must be between 8-128 characters"
+    );
+  }
+
+  alert(`Your password will have ${confirmLength} characters`);
+
+  var confirmSpecialCharacter = confirm(
+    "Click OK to confirm if you would like to include special characters"
+  );
+  var confirmNumericCharacter = confirm(
+    "Click OK to confirm if you would like to include numeric characters"
+  );
+  var confirmLowerCase = confirm(
+    "Click OK to confirm if you would like to include lowercase characters"
+  );
+  var confirmUpperCase = confirm(
+    "Click OK to confirm if you would like to include uppercase characters"
+  );
+
+  while (
+    confirmUpperCase === false &&
+    confirmLowerCase === false &&
+    confirmSpecialCharacter === false &&
+    confirmNumericCharacter === false
+  ) {
+    alert("You must select at least one of the criteria");
+    var confirmSpecialCharacter = confirm(
+      "Click OK to confirm if you would like to include special characters"
+    );
+    var confirmNumericCharacter = confirm(
+      "Click OK to confirm if you would like to include numeric characters"
+    );
+    var confirmLowerCase = confirm(
+      "Click OK to confirm if you would like to include lowercase characters"
+    );
+    var confirmUpperCase = confirm(
+      "Click OK to confirm if you would like to include uppercase characters"
+    );
+  }
+
+  var passwordCharacters = [];
+
+  if (confirmSpecialCharacter) {
+    passwordCharacters = passwordCharacters.concat(specialChars);
+  }
+
+  if (confirmNumericCharacter) {
+    passwordCharacters = passwordCharacters.concat(numbers);
+  }
+
+  if (confirmLowerCase) {
+    passwordCharacters = passwordCharacters.concat(lowercaseABC);
+  }
+
+  if (confirmUpperCase) {
+    passwordCharacters = passwordCharacters.concat(uppercaseABC);
+  }
+
+  var randomPassword = "";
+
+  for (var i = 0; i < confirmLength; i++) {
+    randomPassword =
+      randomPassword +
+      passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+  }
+  return randomPassword;
 }
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -24,5 +162,99 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+// var password = document.querySelector("#password");
+
+// var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// var specialChars = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?"];
+// var lowercaseABC = [
+//   "a",
+//   "b",
+//   "c",
+//   "d",
+//   "e",
+//   "f",
+//   "g",
+//   "h",
+//   "i",
+//   "j",
+//   "k",
+//   "l",
+//   "m",
+//   "n",
+//   "o",
+//   "p",
+//   "q",
+//   "r",
+//   "s",
+//   "t",
+//   "u",
+//   "v",
+//   "w",
+//   "x",
+//   "y",
+//   "z",
+// ];
+// var uppercaseABC = [
+//   "A",
+//   "B",
+//   "C",
+//   "D",
+//   "E",
+//   "F",
+//   "G",
+//   "H",
+//   "I",
+//   "J",
+//   "K",
+//   "L",
+//   "M",
+//   "N",
+//   "O",
+//   "P",
+//   "Q",
+//   "R",
+//   "S",
+//   "T",
+//   "U",
+//   "V",
+//   "W",
+//   "X",
+//   "Y",
+//   "Z",
+// ];
+
+// var confirmLength = "";
+// var confirmSpecialCharacters;
+// var confirmNumericCharacters;
+// var confirmUpperCase;
+// var confirmLowerCase;
+
+// function generatePassword() {
+//   while (confirmLength <= 8 || confirmLength <= 128) {
+//     var confirmLength = prompt(
+//       "Your password must be more than 8 and less then 128 characters"
+//     );
+//     alert(
+//       "Your password must be at least 8 characters and no more than 128 try again"
+//     );
+//   }
+// }
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// https://github.com/TiffanyCasey/Password-Generator/blob/master/script.js
+
+// https://github.com/pkriengsiri/horiseon-code-refactor/blob/main/README.md
